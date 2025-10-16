32a86e6abd51f3132a112dceed82b82d82c5d231
234876 <234876@KB37-118-C4>
2025-10-16 13:29:02 +0300

    Библиотки, класс предметов

--- Pr6/Program.cs
+++ Pr6/Program.cs
@@ -1,1 +1,30 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace GIGACHADGAYDANGEROUS
+{
+   
+    public class Item
+    {
+        public string Name { get; set; }
+        public int Attack { get; set; }
+        public int Defense { get; set; }
+
+        public Item(string name, int attack = 0, int defense = 0)
+        {
+            Name = name;
+            Attack = attack;
+            Defense = defense;
+        }
+    }
+
+    public class Weapon : Item
+    {
+        public Weapon(string name, int attack) : base(name, attack, 0) { }
+    }
+
+    public class Armor : Item
+    {
+        public Armor(string name, int defense) : base(name, 0, defense) { }
+    }
